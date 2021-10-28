4ee083f248b7aa7785a9e01d3e639a7d8f86d23f
ifan2032 <fanleon2002@hotmail.com>
2021-10-28 18:51:27 -0400

    MS revisitions, classify by sample

--- sorted.py
+++ sorted.py
@@ -1,47 +1,38 @@
 import csv
 
-deltaG = 0.15
-deltaA = 0.10
-deltaU = 0.10
-deltaC = 0.05
-delta = 0.05
-deltaI = 0.10
-C, U, G, A, m1G, m2G, I, ho5U = (0.6, 1.2, 3.3, 3.5, 4.67, 4.905, 3.123, 0.90)
+# Exclude everything that starts with cpd 
+
+# Change As Necessary
+deltaG, deltaA, deltaU, deltaC, deltaI, delta = 0.15, 0.10, 0.10, 0.05, 0.4, 0.4
+C, U, G, A, m1G, m2G, I, ho5U = (0.6, 1.2, 3.3, 3.5, 4.67, 4.922, 3.123, 0.90)
+
 rna_values = { (C-deltaC, C+deltaC): 'C', (G-deltaG, G+deltaG): 'G', (A-deltaA, A+deltaA): 'A', (U-deltaU, U+deltaU): 'U'}
-data = { 'C': [], 'G': [], 'A': [], 'U': [], 'm1G': [], 'm2G': [], 'I': [], 'ho5U': []}
+data = { 'Columns': [], 'C': [], 'G': [], 'A': [], 'U': [], 'm1G': [], 'm2G': [], 'I': [], 'ho5U': []}
 ms_filters = {'298.0 -> 166.0': 0, '269.0 -> 137.0': 1, '261.0 -> 129.0': 2}
 ms_values = {(m1G-delta, m1G+delta): 'm1G', (m2G-delta, m2G+delta): 'm2G', (I-deltaI, I+deltaI): 'I', (ho5U-delta, ho5U+delta): 'ho5U'}
 
+files = ["LCMS1//96plate//02_A1_UV_104.csv", "LCMS1//96plate//02_A1_MS_104.csv"]
+
 def parseUV():
-    filename = "LCMS1//101421 Leon practice/02_WT_rRNA_UV_5.csv"
+    filename = files[0]
     
-    # initializing the titles and rows list
     fields = []
     rows = []
     
-    # reading csv file
     with open(filename, 'r') as csvfile:
-        # creating a csv reader object
         csvreader = csv.reader(csvfile)
-        
-        # extracting field names through first row
         fields = next(csvreader)
-
-        # extracting each data row one by one
         for row in csvreader:
             rows.append(row)
         
     divider_indices = []
     for i in range(len(rows)):
         row = rows[i]
-
         if (len(row) == 2):
             divider_indices.append(i)
 
     divider_indices.append(len(rows))
     divider_indices.insert(0, 0)
-
-    print(divider_indices)
 
     time_index, area_index = rows[0].index("RT"), rows[0].index("Area")
 
@@ -49,7 +40,12 @@ def parseUV():
         tmp_data = { (C-deltaC, C+deltaC): '-', (G-deltaG, G+deltaG): '-', (A-deltaA, A+deltaA): '-', (U-deltaU, U+deltaU): '-'}
 
         for row_index in range(divider_indices[index], divider_indices[index+1]):
-            row = rows[row_index] 
+            row = rows[row_index]
+
+            if row_index == divider_indices[index]:
+                info = row[0].split('\\')
+                data['Columns'].append(info[-1].strip())
+
             if not "RT" in row and not "Area" in row and len(row) > max(time_index, area_index):
                 time = float(row[time_index])
                 area = float(row[area_index])
@@ -62,41 +58,22 @@ def parseUV():
             val = tmp_data[(start, end)]
             
             data[rna_values[(start, end)]].append(val)
-
-            
-
-    '''
-    for row in rows[1:]:
-        # parsing each column of a row
-        #for col in row:
-        #    print("%10s"%col),
-        if not "RT" in row and not "Area" in row and len(row) > max(time_index, area_index):
-            time = float(row[time_index])
-            area = float(row[area_index])
-            for (start, end) in rna_values:
-                if start <= time and time <= end:
-                    data[rna_values[(start, end)]].append(area)
-                    break
-    ''' 
-
     return
 
 def parseMS():
-    filename = "LCMS1//101421 Leon practice/02_WT_rRNA_MS_5.csv"
+
+    ms_data = {}
+    for column in data['Columns']:
+        ms_data[column] = { 'm1G': [], 'm2G': [], 'I': [], 'ho5U': [] } # m1G, m2G, I, ho5U
     
-    # initializing the titles and rows list
+    filename = files[1]
     fields = []
     rows = []
     
-    # reading csv file
     with open(filename, 'r') as csvfile:
-        # creating a csv reader object
         csvreader = csv.reader(csvfile)
-        
-        # extracting field names through first row
         fields = next(csvreader)
 
-        # extracting each data row one by one
         for row in csvreader:
             rows.append(row)
 
@@ -115,6 +92,7 @@ def parseMS():
 
     for k in range(len(divider_indices)):
         countera = 0
+        a = 0
         for divider_index in divider_indices[k]:
             countera += 1
             stop_index = divider_indices_two[divider_indices_two.index(divider_index)+1]
@@ -122,7 +100,7 @@ def parseMS():
             isFound = False 
             curRow = 0 
             counter = 0
-
+            a += 1
             for index in range(divider_index+2, stop_index):
                 row = rows[index]
 
@@ -130,59 +108,48 @@ def parseMS():
                     continue
                 rt = float(row[3])
 
+                name = row[0].split('\\')[-1].strip()
+                
                 if k == 0:
                     for (start, end) in list(ms_values.keys())[0:2]:
                         if start <= rt and rt <= end:
                             data[ms_values[(start, end)]].append(float(row[6]))
+                            ms_data[name][ms_values[(start, end)]].append(float(row[6]))
+                            isFound = True
                 elif k == 1:
                     for (start, end) in list(ms_values.keys())[2:3]:
                         if start <= rt and rt <= end and not isFound:
                             data[ms_values[(start, end)]].append(float(row[6]))
+                            ms_data[name][ms_values[(start, end)]].append(float(row[6]))
                             isFound = True
                 else:
                     counter += 1
                     for (start, end) in list(ms_values.keys())[3:4]:
                         if start <= rt and rt <= end and not isFound and float(row[6]) > 200:
                             data[ms_values[(start, end)]].append(float(row[6]))
+                            ms_data[name][ms_values[(start, end)]].append(float(row[6]))
                             isFound = True
 
                 if isFound:
                     break
             
-                
-
-
-
-
-    
-
+    for keys,values in ms_data.items():
+        print(keys)
+        print(values)
 parseUV()
 parseMS()
 
-
-
-# print results
 print("#############---- Results ----##############")
-lengths = []
+lengths = {}
 
-for i in range(len(data["A"])):
-    s = ""
-    for j in ['U', 'G', 'A']:
-        s += f"{data[j][i]}, "
-    s += "\n"
-    print(s)
-
-print(len(data['m1G']), len(data['I']), len(data['ho5U']))
-
-'''
 for pair in data:
-    s = f"{pair} || "
+    s = 0
     for area_val in data[pair]:
-        s += f"{area_val}, "
-    lengths.append(len(data[pair]))
-    s += "\n"
-    print(s)
-print(lengths)'''
+        if area_val == '-':
+            s += 1
+    lengths[pair] = len(data[pair]) - s
+
+print(lengths)
 
 
 with open('results2.csv', 'w') as f:
