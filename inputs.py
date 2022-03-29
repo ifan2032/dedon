0e21d810695bfa58d27f1c4ea055e025366b8d6e
ifan2032 <fanleon2002@hotmail.com>
2022-03-29 11:23:19 -0400

    Bug fixes, fix the double modifications

--- inputs.py
+++ inputs.py
@@ -236,8 +236,10 @@ ms_filters_name = [
 ms_filters_keys = dummy_keys
 ms_filters_name = dummy_name
 
+'''
 for i in range(len(ms_filters_keys)):
     print(ms_filters_keys[i], ms_filters_name[i])
+'''
 
 # directly checks if input values are valid, else throws error
 if len(rows[0]) != len(rows[1]) or len(rows[1]) != len(rows[2]):
